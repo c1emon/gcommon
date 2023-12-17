55c1afe3a8dd820b6b24a8ffd9684451704c6479
clemon <cjw7360chen@gmail.com>
2023-12-17 18:44:39 +0800

    add health endpoint

--- cloud/endpoint.go
+++ cloud/endpoint.go
@@ -34,7 +34,17 @@ type Endpoint struct {
 	// Host is the ip/domain of endpoint
 	Host string `json:"host"`
 	Port int    `json:"port"`
-	// deal with local ca
+	// Path of http url
+	Path string `json:"path"`
+	// Secure control if use ssl
 	Secure bool `json:"secure"`
+	// TlsCfg tls/ssl config
 	TlsCfg *tls.Config
+}
+
+type HealthEndpoint struct {
+	Endpoint
+	Heartbeat                      bool
+	HealthCheckInterval            int
+	DeregisterCriticalServiceAfter int
 }
